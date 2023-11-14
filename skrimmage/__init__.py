fc7e52794af622d1c254f86004355c65cef81348
Danny Page <dannypage@users.noreply.github.com>
2023-11-14 12:16:45 -0500

    Updating assets

--- skrimmage/__init__.py
+++ skrimmage/__init__.py
@@ -1,4 +1,4 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, AssetSelection, define_asset_job, load_assets_from_modules, ScheduleDefinition
 
 from .assets import f1, nhl, frontend
 from . import metrics
@@ -6,11 +6,22 @@ from .resources import database_resource, ErgastDatabaseResource, NhlApiResource
 
 all_assets = load_assets_from_modules([f1, nhl, metrics, frontend])
 
+daily_refresh_job = define_asset_job("daily_refresh_job", selection=AssetSelection.all())
+
+daily_refresh_schedule = ScheduleDefinition(
+    job=daily_refresh_job,
+    cron_schedule="0 8 * * *"
+)
+
 defs = Definitions(
     assets=all_assets,
+    jobs=[daily_refresh_job],
     resources={
     	"database": database_resource,
         "nhl_api": NhlApiResource,
         "ergast_f1_api": ErgastDatabaseResource
     },
+    schedules=[daily_refresh_schedule],
 )
+
+
